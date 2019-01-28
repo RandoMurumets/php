a370c6c7dd0f5b77656c83ca8435abd747e3b179
rando <rando.murumets@gmail.com>
2019-01-28 12:44:43 +0200

    commit

---
+++ Variables/index.php
@@ -0,0 +1,23 @@
+<?php
+
+// muutujate defineerimine;
+// $muutujanimi = väärtus;
+    echo '<h4>Matemaatilised operaatorid</h4>';
+    $arv1 = 7;
+    $arv2 = 24;
+
+        echo $arv1.' + '.$arv2.' = '.($arv1 + $arv2).'<br>';
+        echo $arv1.' - '.$arv2.' = '.($arv1 - $arv2).'<br>';
+        echo $arv1.' * '.$arv2.' = '.($arv1 * $arv2).'<br>';
+        echo $arv1.' / '.$arv2.' = '.($arv1 / $arv2).'<br>';
+        echo $arv1.' % '.$arv2.' = '.($arv1 % $arv2).'<br>';
+        echo '<hr>';
+        echo '<h4>Võrdlusoperaatorid</h4>';
+        echo $arv1.' < '.$arv2.' see on '.($arv1 < $arv2).'<br>';
+        echo $arv1.' > '.$arv2.' see on '.($arv1 > $arv2).'<br>';
+        echo $arv1.' <= '.$arv2.' see on '.($arv1 <= $arv2).'<br>';
+        echo $arv1.' >= '.$arv2.' see on '.($arv1 >= $arv2).'<br>';
+        echo $arv1.' == '.$arv2.' see on '.($arv1 == $arv2).'<br>';
+        echo $arv1.' != '.$arv2.' see on '.($arv1 != $arv2).'<br>';
+
+?>
