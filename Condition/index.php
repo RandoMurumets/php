62dc560ef46b7b638a09b040425f30623ce708e9
rando <rando.murumets@gmail.com>
2019-01-28 13:07:45 +0200

    commit

---
+++ Condition/index.php
@@ -0,0 +1,24 @@
+<?php
+// Tingimuslaused
+// if (tingimus) {
+// tegevused, kui tingimus kehtib
+// } else {
+// tegevused, kui tingimus ei kehti
+// }
+echo '<h4>Tingimuslaused</h4>';
+$foor = 'sinine';
+switch ($foor) {
+    case 'punane':
+        echo '<p style="color: red">Punane</p>';
+        break;
+    case 'kollane':
+        echo '<p style="color: yellow">Kollane</p>';
+        break;
+    case 'roheline':
+        echo '<p style="color: green">Roheline</p>';
+        break;
+    default:
+        echo '<p>Mingi teine</p>';
+        break;
+}
+    ?>
