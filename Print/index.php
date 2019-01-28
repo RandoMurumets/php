6052364c516b7ca5af1a29ed116c14f7567e9612
rando <rando.murumets@gmail.com>
2019-01-28 13:21:19 +0200

    commit

---
+++ Print/index.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>PHP alused - Väljastus</title>
+    </head>
+    <body>
+        <h4>Väljastus</h4>
+        <?php
+        echo 'See tekst on HTML sees'.'<br>'
+        ?>
+    </body>
+</html>
